563afb57a1e44150702fb38c123568311a3dea53
kei198 <keiichi.kakizawa@gmail.com>
2024-11-05 22:23:17 +0900

    gradleのdependenciesにorg.apache.commonsを追加

--- src/main/java/raisetech/studnet/management/Application.java
+++ src/main/java/raisetech/studnet/management/Application.java
@@ -1,7 +1,9 @@
 package raisetech.studnet.management;
 
+import io.micrometer.common.util.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +11,11 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello,Wold!";
+	}
+
 
 }

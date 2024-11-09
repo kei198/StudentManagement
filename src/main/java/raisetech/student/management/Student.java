1f224c8b21edde97a79fccd3c99d70f403d53682
kei198 <keiichi.kakizawa@gmail.com>
2024-11-10 00:45:01 +0900

    いったんコミット

--- src/main/java/raisetech/student/management/Student.java
+++ src/main/java/raisetech/student/management/Student.java
@@ -1,22 +1,12 @@
 package raisetech.student.management;
 
 public class Student {
+  private String id;
   private String name;
+  private String ruby;
+  private String mailaddress;
+  private String address;
   private int age;
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
+  private String gender;
+  private String nickname;
 }

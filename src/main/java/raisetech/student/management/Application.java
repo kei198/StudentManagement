45f531b5604b7aa598fe83fcff5c96ad052c2332
kei198 <keiichi.kakizawa@gmail.com>
2024-11-10 17:42:00 +0900

    24_Read処理のServiceとController部分を実装

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,16 +1,12 @@
 package raisetech.student.management;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import raisetech.student.management.repository.StudentRepository;
 
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	@Autowired
@@ -18,16 +14,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@GetMapping("/studentsList")
-	public List<Student> getStudents(){
-		return repository.getAllStudents() ;
-	}
-
-	@GetMapping("/studentsCoursesList")
-	public List<StudentCourse> getStudentsCourses(){
-		return repository.getAllStudentsCourses();
 	}
 
 }
---
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -0,0 +1,28 @@
+package raisetech.student.management.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.repository.StudentRepository;
+
+@Service
+public class StudentService {
+  private StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> serchStudentsList(){
+    return repository.searchStudents() ;
+  }
+
+  public List<StudentCourse> searchStudentsCoursesList(){
+    return repository.searchStudentsCourses();
+  }
+
+
+}
